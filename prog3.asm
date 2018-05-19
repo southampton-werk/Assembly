909c89b45580446569974431c713450402242ad7
Jamie Scott <github@jami.org.uk>
2018-05-19 17:54:34 +0100

    actually fixed all the things

--- prog3.asm
+++ prog3.asm
@@ -5,17 +5,15 @@ IoStatusBlock$ = 96
 
 main	PROC
 	mov	ecx, -11				; fffffff5H
-	;call GetStdHandle
-	mov rax, QWORD PTR gs:[96]
-	mov rcx, QWORD PTR [rax+32]
-	mov rbx, QWORD PTR [rcx+40] ; This effectively gets 0x4C (76)
-	mov rax, rbx
-	;end GetStdHandle
-	mov	rcx, rax
-	mov	QWORD PTR [rsp+64], 0
-	mov	DWORD PTR [rsp+56], 0
-	mov	eax, 19  ; 13
-	mov	DWORD PTR [rsp+48], eax
+	mov rax, gs:[96]
+	mov rcx, [rax+32]
+	mov rbx, [rcx+40] ; This effectively gets 0x4C (76)
+	mov	rcx, rbx
+	xor rax, rax
+	mov rax, QWORD PTR[rsp+64]
+	mov QWORD PTR [rsp+64], rax
+	mov DWORD PTR [rsp+56], eax
+	mov	DWORD PTR [rsp+48], 19
 	lea	rax, QWORD PTR pwned
 	mov	QWORD PTR [rsp+40], rax
 	lea	rax, QWORD PTR IoStatusBlock$[rsp]
@@ -23,19 +21,15 @@ main	PROC
 	xor	r9d, r9d
 	xor	r8d, r8d
 	xor	edx, edx
-	;call NtWriteFile
-	push rax  ; Fix stack alignment by adding 8 bytes
+	sub rsp, 8  ; Fix stack alignment
 	mov r10, rcx
 	mov eax, 8
 	syscall
-	pop rax  ; Undo fix
-	ret
-	;end NtWriteFile
+	add rsp, 8  ; Undo fix
 	ret	0
 main	ENDP
 
-pwned:
-	DB	'You''ve been pwned', 0dH, 0aH, 00H
+pwned DB 'You''ve been pwned', 0dH, 0aH, 00H
 
 _TEXT	ENDS
 END
